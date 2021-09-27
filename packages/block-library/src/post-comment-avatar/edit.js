d401fd89993c040198b70aa18d3384743a2bc911
Carlos Bravo <carlos@ticloop.com>
2021-09-27 16:28:36 +0200

    Added border radius and size of post comment avatar block

---
+++ packages/block-library/src/post-comment-avatar/edit.js
@@ -0,0 +1,97 @@
+/**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
+ * WordPress dependencies
+ */
+import { useSelect } from '@wordpress/data';
+import {
+	InspectorControls,
+	__experimentalUseBorderProps as useBorderProps,
+	__experimentalImageSizeControl as ImageSizeControl,
+	useBlockProps,
+} from '@wordpress/block-editor';
+import { store as coreStore } from '@wordpress/core-data';
+import { PanelBody, ResizableBox } from '@wordpress/components';
+import { __, isRTL } from '@wordpress/i18n';
+
+export default ( { attributes, context, setAttributes } ) => {
+	const { className, style, height, width } = attributes;
+	const { commentId } = context;
+	const maxImageWidth = 96;
+	const maxImageHeight = 96;
+	if ( ! height || ! width ) {
+		setAttributes( {
+			height: maxImageHeight,
+			width: maxImageWidth,
+		} );
+	}
+
+	const displayAvatar = useSelect( ( select ) => {
+		const { getEntityRecord } = select( coreStore );
+		const comment = getEntityRecord( 'root', 'comment', commentId );
+		const authorAvatarUrls = comment?.author_avatar_urls; // eslint-disable-line camelcase
+		const borderProps = useBorderProps( attributes );
+
+		if ( authorAvatarUrls ) {
+			const avatarUrls = Object.values( authorAvatarUrls );
+			const biggestAvatarUrl = avatarUrls[ 2 ];
+			return (
+				<ResizableBox
+					size={ {
+						width: width || maxImageWidth,
+						height: height || maxImageHeight,
+					} }
+					onResizeStop={ ( event, direction, elt, delta ) => {
+						setAttributes( {
+							height: parseInt( height + delta.height, 10 ),
+							width: parseInt( width + delta.width, 10 ),
+						} );
+					} }
+					lockAspectRatio
+					enable={ {
+						top: false,
+						right: isRTL() ? false : true,
+						bottom: true,
+						left: isRTL() ? true : false,
+					} }
+				>
+					<img
+						className={ classnames(
+							className,
+							borderProps.className,
+							{
+								// For backwards compatibility add style that isn't
+								// provided via block support.
+								'no-border-radius': style?.border?.radius === 0,
+							}
+						) }
+						style={ {
+							...borderProps.style,
+						} }
+						src={ biggestAvatarUrl }
+						alt="Comment Author Avatar"
+					/>
+				</ResizableBox>
+			);
+		}
+	} );
+	return (
+		<>
+			<InspectorControls>
+				<PanelBody title={ __( 'Comment Avatar Settings' ) }>
+					<ImageSizeControl
+						onChange={ ( value ) => setAttributes( value ) }
+						width={ width }
+						height={ height }
+						imageWidth={ maxImageWidth }
+						imageHeight={ maxImageHeight }
+					/>
+				</PanelBody>
+			</InspectorControls>
+			<div { ...useBlockProps() }>{ displayAvatar }</div>
+		</>
+	);
+};
---
+++ packages/block-library/src/post-comment-avatar/index.js
@@ -0,0 +1,20 @@
+/**
+ * Internal dependencies
+ */
+import metadata from './block.json';
+import edit from './edit';
+import save from './save';
+
+/**
+ * WordPress dependencies
+ */
+import { postAuthor as icon } from '@wordpress/icons';
+
+const { name } = metadata;
+export { metadata, name };
+
+export const settings = {
+	icon,
+	edit,
+	save,
+};
