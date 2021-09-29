d6fe6c8ace12f366b750fcf4b4d1d74cafbe57c0
Carlos Bravo <carlos@ticloop.com>
2021-09-29 13:12:12 +0200

    Fix style to get max-width, refactor avatar useSelect

--- packages/block-library/src/post-comment-avatar/edit.js
+++ packages/block-library/src/post-comment-avatar/edit.js
@@ -17,59 +17,22 @@ import { store as coreStore } from '@wordpress/core-data';
 import { useSelect } from '@wordpress/data';
 import { __, isRTL } from '@wordpress/i18n';
 
-export default ( { attributes, context, setAttributes } ) => {
+export default ( { attributes, context: { commentId }, setAttributes } ) => {
 	const { className, style, height, width, alt } = attributes;
-	const { commentId } = context;
-
-	const displayAvatar = useSelect( ( select ) => {
-		const { getEntityRecord } = select( coreStore );
-		const comment = getEntityRecord( 'root', 'comment', commentId );
-		const authorAvatarUrls = comment?.author_avatar_urls; // eslint-disable-line camelcase
-		const borderProps = useBorderProps( attributes );
-
-		if ( authorAvatarUrls ) {
-			const avatarUrls = Object.values( authorAvatarUrls );
-
-			return (
-				<ResizableBox
-					size={ {
-						width,
-						height,
-					} }
-					onResizeStop={ ( event, direction, elt, delta ) => {
-						setAttributes( {
-							height: parseInt( height + delta.height, 10 ),
-							width: parseInt( width + delta.width, 10 ),
-						} );
-					} }
-					lockAspectRatio
-					enable={ {
-						top: false,
-						right: isRTL() ? false : true,
-						bottom: true,
-						left: isRTL() ? true : false,
-					} }
-				>
-					<img
-						className={ classnames(
-							className,
-							borderProps.className,
-							{
-								// For backwards compatibility add style that isn't
-								// provided via block support.
-								'no-border-radius': style?.border?.radius === 0,
-							}
-						) }
-						style={ {
-							...borderProps.style,
-						} }
-						src={ avatarUrls[ 2 ] }
-						alt={ alt }
-					/>
-				</ResizableBox>
-			);
-		}
-	} );
+	const { comment } = useSelect(
+		( select ) => {
+			const { getEntityRecord } = select( coreStore );
+			return {
+				comment: getEntityRecord( 'root', 'comment', commentId ),
+			};
+		},
+		[ commentId ]
+	);
+	const borderProps = useBorderProps( attributes );
+	const authorAvatarUrls = comment?.author_avatar_urls; // eslint-disable-line camelcase
+	const avatarUrls = authorAvatarUrls
+		? Object.values( authorAvatarUrls )
+		: null;
 	return (
 		<>
 			<InspectorControls>
@@ -83,7 +46,47 @@ export default ( { attributes, context, setAttributes } ) => {
 					/>
 				</PanelBody>
 			</InspectorControls>
-			<div { ...useBlockProps() }>{ displayAvatar }</div>
+			<div { ...useBlockProps() }>
+				{ avatarUrls ? (
+					<ResizableBox
+						size={ {
+							width,
+							height,
+						} }
+						onResizeStop={ ( event, direction, elt, delta ) => {
+							setAttributes( {
+								height: parseInt( height + delta.height, 10 ),
+								width: parseInt( width + delta.width, 10 ),
+							} );
+						} }
+						lockAspectRatio
+						enable={ {
+							top: false,
+							right: isRTL() ? false : true,
+							bottom: true,
+							left: isRTL() ? true : false,
+						} }
+					>
+						<img
+							className={ classnames(
+								className,
+								borderProps.className,
+								{
+									// For backwards compatibility add style that isn't
+									// provided via block support.
+									'no-border-radius':
+										style?.border?.radius === 0,
+								}
+							) }
+							style={ {
+								...borderProps.style,
+							} }
+							src={ avatarUrls[ 2 ] }
+							alt={ alt }
+						/>
+					</ResizableBox>
+				) : null }
+			</div>
 		</>
 	);
 };
